c6268f34511e3aaf9a816a258d2db5117967bde6
Terra111111 <86809497+Terra111111@users.noreply.github.com>
2024-05-19 22:49:19 -0700

    Hour 20

---
+++ JGraham_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,18 @@
+/* Is the game too easy or hard?
+ - I think that it's out of the book difficulty is too easy. I made various changes to ramp of the difficulty.
+
+I changed the overall speed of which includes the players movement speed, the rate at which obstacles and powerups spawn. I also changed how much time you get from powerups and how much you loose from obstacles.
+
+List 2 things that would give it a "wow" factor.
+
+- I think something that would give it a wow factor is changing the backround and adding a more detailed platform. 
+
+What 2 parts of the game are fun? 
+
+- I think the most fun parts are not hitting an obstacle streak and trying to see how long you can survive. 
+I believe I mostly fixed this by tweaking the values of time gained and lost so you can't just keep hitting obstacles over and over and still gain time.
+
+What 2 parts of the game are tedious?
+- I believe that one of the issues I fixed of being able to basically not loose because of how much time you gain from powerups.
+as well as the overall speed of the game feeling monotonous. Both of these I feel I have fixed to the best of my ability. As well as fixing the powerup and obstacle model to be more unique. 
+*/
